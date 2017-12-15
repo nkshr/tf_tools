b3d23f9c8ad49361059dee5c23a5bc7d97d2b49f
nkshr <naokasa@outlook.jp>
2017-12-15 16:31:41 +0900

    first commit

---
+++ lib/eval_info.py
@@ -0,0 +1,431 @@
+import numpy as np
+import os
+import csv
+import sys
+import random
+
+class eval_info:
+    def init(self, labeled_images_file, labels_file, num_images):
+        with open(labels_file, 'r') as f:
+            labels = [line.replace('\n', '')  for line in f.readlines()];
+
+        self.cinfo_list = []
+        self.top1_rate = 0
+        self.top5_rate = 0
+        
+        class_id = 0
+        for label in labels:
+            cinfo = class_info()
+            cinfo.label = label
+            cinfo.class_id = class_id
+            self.cinfo_list.append(cinfo)
+            class_id += 1
+            
+        with open(labeled_images_file, 'r') as f:
+            for line in f.readlines():                                    
+                iinfo = image_info()
+                
+                toks = line.split(sep=" ", maxsplit=1)
+                idx = int(toks[1].replace('\n', ''))
+                iinfo.name = toks[0]
+
+                cinfo = self.cinfo_list[idx]
+                if len(cinfo.iinfo_list) < num_images or num_images < 0:
+                    cinfo.iinfo_list.append(iinfo)
+                #self.__cinfo_list[idx].images.append(image)
+
+            for cinfo in self.cinfo_list:
+                if len(cinfo.iinfo_list) != num_images and num_images >= 0:
+                    print('class', cinfo.class_id, 'has too less images({}).'.format(len(cinfo.iinfo_list)))
+
+    def __init__(self):
+        self.cinfo_list = []
+        self.top1_rate = 0
+        self.top5_rate = 0
+                    
+    def get_class_count(self):
+        return len(self.cinfo_list)
+
+    def get_class_info(self, class_id):
+        for cinfo in self.cinfo_list:
+            if cinfo.class_id == class_id:
+                return cinfo
+
+    def calc_topk_rate(self, k):
+        num_topk = 0
+        num_images = 0
+        for cinfo in self.cinfo_list:
+            for iinfo in cinfo.iinfo_list:
+                if not iinfo.rank < 0:
+                    num_images += 1
+                    if iinfo.rank < k and iinfo.rank >= 0:
+                        num_topk += 1
+
+        return num_topk / num_images
+    
+    def calc_top5_rate(self):
+        num_correct_preds = 0
+        num_images = 0
+        for cinfo in self.cinfo_list:
+            for iinfo in cinfo.iinfo_list:
+                num_images += 1
+                if iinfo.rank < 5:
+                    num_correct_preds += 1
+
+        if  num_images:
+            self.top5_rate = num_correct_preds / num_images
+        else:
+            self.top5_rate = 0
+            
+    def calc_top1_rate(self):        
+        num_correct_preds = 0
+        num_images = 0
+        
+        for cinfo in self.cinfo_list:
+            num_images += len(cinfo.iinfo_list)
+            for iinfo in cinfo.iinfo_list:
+                if iinfo.rank == 0:
+                    num_correct_preds += 1
+        if num_images:
+            self.top1_rate = num_correct_preds / num_images
+        else:
+            self.top1_rate = 0
+            
+    def __iter__(self):
+        self.cinfo_list_idx = 0
+        return self        
+
+    def __next__(self):
+        if self.cinfo_list_idx == len(self.cinfo_list):
+            raise StopIteration
+        self.cinfo_list_idx += 1
+        return self.cinfo_list[self.cinfo_list_idx-1]
+
+    def sort_by_top1_rate(self):
+        top1_rate_arr = np.array([cinfo.top1_rate for cinfo in self.cinfo_list])
+        sorted_indexes = np.argsort(top1_rate_arr)[::-1]
+        sorted_cinfo_list = [self.cinfo_list[index] for index in sorted_indexes]
+        self.cinfo_list  = sorted_cinfo_list
+
+    def sort_by_class_id(self):
+        class_ids = np.array([cinfo.class_id for cinfo in self.cinfo_list])
+        sorted_indexes = np.argsort(class_ids)
+        sorted_cinfo_list = [self.cinfo_list[index] for index in sorted_indexes]
+        self.cinfo_list = sorted_cinfo_list
+
+    def calc_top1_rate_rank(self):
+        top1_rate_arr = np.array([cinfo.top1_rate for cinfo in self.cinfo_list])
+        sorted_indexes = np.argsort(top1_rate_arr)[::-1]
+        for rank in range(len(sorted_indexes)):
+            self.cinfo_list[sorted_indexes[rank]].top1_rate_rank = rank
+
+    def calc_top5_rate_rank(self):
+        top5_rate_arr = np.array([cinfo.top5_rate for cinfo in self.cinfo_list])
+        sorted_indexes = np.argsort(top5_rate_arr)[::-1]
+        for rank in range(len(sorted_indexes)):
+            self.cinfo_list[sorted_indexes[rank]].top5_rate_rank = rank
+
+    def write_summary(self, fname):
+        text = 'top1_rate,{}\n'.format(self.top1_rate)
+        text += 'top5_rate,{}\n'.format(self.top5_rate)
+        
+        text += 'class_id,label,num_images,top1_rate,top5_rate,top1_rate_rank,top5_rate_rank\n'
+        for cinfo in self.cinfo_list:
+            text += '{},\"{}\",{},{},{},{},{}\n'.format(cinfo.class_id, cinfo.label, len(cinfo.iinfo_list), cinfo.top1_rate, cinfo.top5_rate, cinfo.top1_rate_rank, cinfo.top5_rate_rank)
+
+        with open(fname, 'w') as f:
+            f.write(text)
+            f.flush()
+
+    def write(self, dir):
+        summary = os.path.join(dir, 'summary.csv')
+        self.write_summary(summary)
+
+        for cinfo in self:
+            detail = os.path.join(dir, 'class{0:04d}.csv'.format(cinfo.class_id))
+            cinfo.write(detail)
+
+    def read_summary(self, fname):
+        self.top1_rate = 0
+        self.top5_rate = 0
+
+        with open(fname, 'r') as f:
+            reader  = csv.reader(f)
+            row = next(reader)
+            self.top1_rate = row[1]
+
+            row = next(reader)
+            self.top5_rate = row[1]
+
+    def read(self, dir):
+        self.cinfo_list = []
+        self.top1_rate = 0
+        self.top5_rate = 0
+        
+        summary = os.path.join(dir, 'summary.csv')
+        self.read_summary(summary)
+        
+        class_csvs = [class_csv for class_csv in os.listdir(dir) if class_csv.startswith('class')]
+
+        for class_csv in class_csvs:
+            cinfo = class_info()
+            cinfo.read(os.path.join(dir, class_csv))
+            self.cinfo_list.append(cinfo)
+
+    def take_statistcs(self):
+        for cinfo in self.cinfo_list:
+            cinfo.take_statistics()
+        
+        self.top1_rate = self.calc_topk_rate(1)
+        self.top5_rate = self.calc_topk_rate(5)
+        self.calc_top1_rate_rank()
+        self.calc_top5_rate_rank()
+
+#######################class_info#######################
+class class_info:
+    def __init__(self):
+        self.iinfo_list = []
+        self.label = ''
+        self.class_id = ''
+        self.top1_rate = -1
+        self.top5_rate = -1
+        self.top1_rate_rank = -1
+        self.top5_rate_rank = -1
+        self.evaluated = False
+        
+    def calc_top1_rate(self):
+        if (len(self.iinfo_list)) == 0:
+            self.top1_rate = -1
+            return
+
+        if iinfo_list[0].rank < -1:
+            self.top1_rate = -1
+            return 
+
+        num_top1 = 0
+        for iinfo in self.iinfo_list:
+            if iinfo.rank == 0:
+                num_top1 += 1
+        self.top1_rate = num_top1 / len(self.iinfo_list)
+
+    def calc_top5_rate(self):
+        if (len(self.iinfo_list)) == 0:
+            self.top5_rate = -1
+            return
+
+        if iinfo_list[0].rank < -1:
+            self.top1_rate = -1
+            return
+        
+        num_top5 = 0
+        for iinfo in self.iinfo_list:
+            if iinfo.rank < 5 and iinfo.rank > 0:
+                num_top5 += 1
+        self.top5_rate = num_top5 / len(self.iinfo_list)
+
+    def calc_topk_rate(self, k):
+        if (len(self.iinfo_list)) == 0:
+            return -1
+
+        if self.iinfo_list[0].rank < 0:
+            return -1
+        
+        num_topk = 0
+        for iinfo in self.iinfo_list:
+            if iinfo.rank < k and iinfo.rank >= 0:
+                num_topk += 1
+
+        return num_topk / len(self.iinfo_list)
+
+    def write(self, fname):
+        text = 'class_id,{}\n'.format(self.class_id)
+        text += 'label,\"{}\"\n'.format(self.label)
+        text += 'top1_rate,{}\n'.format(self.top1_rate)
+        text += 'top5_rate,{}\n'.format(self.top5_rate)
+        text += 'top1_rate_rank,{}\n'.format(self.top1_rate_rank)
+        text += 'top5_rate_rank,{}\n'.format(self.top5_rate_rank)
+        text += 'image,rank,rank_in_class,prob,1,,2,,3,,4,,5\n'
+        
+        with open(fname, 'w') as f:
+            f.write(text)
+            f.flush()
+
+            for iinfo in self.iinfo_list:
+                text = '{},{},{},{},'.format(
+                    iinfo.name,
+                    iinfo.rank,
+                    iinfo.rank_in_class,
+                    iinfo.prob
+                )
+
+                for i in range(5):
+                    class_id = iinfo.top5[i]['class_id']
+                    prob = iinfo.top5[i]['prob']
+                    text += '{},{},'.format(class_id, prob)
+
+                text += '\n'
+
+                f.write(text)
+                f.flush()
+
+    def sort_by_prob(self):
+        probs = [iinfo.prob for iinfo in self.iinfo_list]
+        sorted_indexes = np.argsort(probs)[::-1]
+        sorted_iinfo_list = [iinfo_list[index] for index in sorted_indexes]
+        self.iinfo_list = sorted_info_list
+
+    def calc_rank_in_class(self):
+        probs = []
+        for iinfo in self.iinfo_list:
+            probs.append(iinfo.prob)
+
+        sorted_indexes = np.argsort(probs)[::-1]
+        for rank in range(len(sorted_indexes)):
+            self.iinfo_list[sorted_indexes[rank]].rank_in_class = rank
+
+    def read(self, fname):
+        iinfo_list = []
+        
+        with open(fname, 'r') as f:
+            reader = csv.reader(f)
+            toks = next(reader)
+            self.class_id = int(toks[1])
+
+            toks = next(reader)
+            self.label = toks[1]
+
+            toks = next(reader)
+            self.top1_rate = float(toks[1])
+
+            toks = next(reader)
+            self.top5_rate = float(toks[1])
+
+            toks = next(reader)
+            self.top1_rate_rank = int(toks[1])
+
+            toks = next(reader)
+            self.top5_rate_rank = int(toks[1])
+            
+            next(reader) #skip header
+            for row in reader:
+                iinfo = image_info()
+                iinfo.name = row[0]
+                iinfo.rank = int(row[1])
+                iinfo.rank_in_class = int(row[2])
+                iinfo.prob = float(row[3])
+                for i in range(5):
+                    class_id = row[4+i*2]
+                    prob = row[5+i*2]
+                    iinfo.top5[i]['class_id'] = class_id
+                    iinfo.top5[i]['prob'] = prob
+
+                self.iinfo_list.append(iinfo)
+                
+    def take_statistics(self):
+        self.top1_rate = self.calc_topk_rate(1)
+        self.top5_rate = self.calc_topk_rate(5)
+        self.calc_rank_in_class()
+
+    def calc_top5_mispreds(self):
+        mispred_votes = [0 for i in self.get_class_count()]
+        mispred_votes[self.class_id] = -1
+        
+        for iinfo in iinfo_list:
+            for rank in range(5):
+                class_id = iinfo.top5[rank]['class_id']
+                if class_id == self.class_id:
+                    break
+
+                point = 5 - class_id
+                mispred_votes[class_id] += point
+
+        sorted_indexes = np.argsort(mispred_votes)[::-1]
+        self.top5_mispreds = [sorted_indexes[rank] for rank in range(5)]
+        
+###################image_info##################
+class image_info:
+    def __init__(self):
+        self.name=''
+        self.rank = -1
+        self.rank_in_class = -1
+        self.prob=-1
+        self.top5=[{'class_id' : -1, 'prob' : -1} for i in range(5)] #class_id, prob
+
+##################eval_info_comp###############
+class eval_info_comp:
+    def __init__(self):
+        pass
+
+    def read(self, left, right):
+        self.left_einfo = eval_info.eval_info()
+        self.left_einfo.read(left)
+        self.left_einfo.sort_by_class_id()
+        
+        self.right_einfo = eval_info.eval_info()
+        self.right_einfo.read(right)
+        self.right_einfo.sort_by_class_id()
+
+        if self.left_einfo.get_class_count() != self.right_einfo.get_class_count():
+            raise ValueError('{}({}) and {}({}) have same number of classes.'.format(left, left_einfo.get_class_count(), right,right_einfo.get_class_count()))
+                
+    def take_synthesis(self):
+        self.roc_list = [-1 for i in range(self.right_einfo.get_class_count())]
+
+        for class_id in range(self.left_einfo.get_class_count()):
+            left_cinfo = self.left_einfo.get_class_info(class_id)
+            right_cinfo = self.right_einfo.get_class_info(class_id)
+            if left_cinfo.top1_rate < sys.float_info.epsilon or right_cinfo.top1_rate < 0:
+                self.roc_list[class_id] = -1
+            else:
+                self.roc_list[class_id] = (right_cinfo.top1_rate / left_cinfo.top1_rate)
+
+        self.sum_roc = 0
+        self.ave = 0
+        num_valid_roc = 0
+        
+        for roc in self.roc_list:
+            if roc >= 0:
+                self.sum_roc += roc
+                num_valid_roc += 1
+        self.ave = self.sum_roc / num_valid_roc
+
+        self.sdev = 0
+        for roc in self.roc_list:
+            if roc >= 0:
+                self.sdev += pow(roc - self.ave, 2.0)
+        self.sdev = math.sqrt(self.sdev / num_valid_roc)
+
+        num_roc_under1 = 0
+        
+        for roc in self.roc_list:
+            if roc < 0:
+                continue
+            if roc < 1:
+                num_roc_under1 += 1
+        
+        self.roc_under1_rate = num_roc_under1 / num_valid_roc
+        pass
+    
+    def write(self, fname):
+        with open(fname, 'w') as f:
+            writer = csv.writer(f, delimiter=',', quotechar='"')
+            
+            writer.writerow(['roc_under1_rate', self.roc_under1_rate])
+            writer.writerow(['ave', self.ave])
+            writer.writerow(['sdev', self.sdev])
+
+            writer.writerow(['class_id','label', 'top1_rate', 'top1_rate', 'roc'])
+            sorted_indexes = np.argsort(self.roc_list)
+            for class_id in sorted_indexes:
+                left_cinfo = self.left_einfo.get_class_info(class_id)
+                right_cinfo = self.right_einfo.get_class_info(class_id)
+                roc = self.roc_list[class_id]
+                writer.writerow([class_id, left_cinfo.label, left_cinfo.top1_rate, right_cinfo.top1_rate, roc])
+
+        pass
+
+    def write_left_einfo(self, fname):
+        self.left_einfo.write(fname)
+
+    def write_right_einfo(self, fname):
+        self.right_einfo.write(fname)
---
+++ lib/util.py
@@ -0,0 +1,320 @@
+import argparse
+from datetime import datetime
+import hashlib
+import os.path
+import random
+import re
+import sys
+import tarfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.platform import gfile
+from tensorflow.python.util import compat
+
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
+
+def add_final_training_ops(class_count, final_tensor_name, bottleneck_tensor,
+                           bottleneck_tensor_size, learning_rate):
+  """Adds a new softmax and fully-connected layer for training.
+
+  We need to retrain the top layer to identify our new classes, so this function
+  adds the right operations to the graph, along with some variables to hold the
+  weights, and then sets up all the gradients for the backward pass.
+
+  The set up for the softmax and fully-connected layers is based on:
+  https://www.tensorflow.org/versions/master/tutorials/mnist/beginners/index.html
+
+  Args:
+    class_count: Integer of how many categories of things we're trying to
+    recognize.
+    final_tensor_name: Name string for the new final node that produces results.
+    bottleneck_tensor: The output of the main CNN graph.
+    bottleneck_tensor_size: How many entries in the bottleneck vector.
+
+  Returns:
+    The tensors for the training and cross entropy results, and tensors for the
+    bottleneck input and ground truth input.
+  """
+  with tf.name_scope('input'):
+    bottleneck_input = tf.placeholder_with_default(
+        bottleneck_tensor,
+        shape=[None, bottleneck_tensor_size],
+        name='BottleneckInputPlaceholder')
+
+    ground_truth_input = tf.placeholder(tf.float32,
+                                        [None, class_count],
+                                        name='GroundTruthInput')
+
+  # Organizing the following ops as `final_training_ops` so they're easier
+  # to see in TensorBoard
+  layer_name = 'final_training_ops'
+  with tf.name_scope(layer_name):
+    with tf.name_scope('weights'):
+      initial_value = tf.truncated_normal(
+          [bottleneck_tensor_size, class_count], stddev=0.001)
+
+      layer_weights = tf.Variable(initial_value, name='final_weights')
+
+      variable_summaries(layer_weights)
+    with tf.name_scope('biases'):
+      layer_biases = tf.Variable(tf.zeros([class_count]), name='final_biases')
+      variable_summaries(layer_biases)
+    with tf.name_scope('Wx_plus_b'):
+      logits = tf.matmul(bottleneck_input, layer_weights) + layer_biases
+      tf.summary.histogram('pre_activations', logits)
+
+  final_tensor = tf.nn.softmax(logits, name=final_tensor_name)
+  tf.summary.histogram('activations', final_tensor)
+
+  with tf.name_scope('cross_entropy'):
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+        labels=ground_truth_input, logits=logits)
+    with tf.name_scope('total'):
+      cross_entropy_mean = tf.reduce_mean(cross_entropy)
+  tf.summary.scalar('cross_entropy', cross_entropy_mean)
+
+  with tf.name_scope('train'):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_step = optimizer.minimize(cross_entropy_mean)
+
+  return (train_step, cross_entropy_mean, bottleneck_input, ground_truth_input,
+          final_tensor)
+
+
+def add_evaluation_step(result_tensor, ground_truth_tensor):
+  """Inserts the operations we need to evaluate the accuracy of our results.
+
+  Args:
+    result_tensor: The new final node that produces results.
+    ground_truth_tensor: The node we feed ground truth data
+    into.
+
+  Returns:
+    Tuple of (evaluation step, prediction).
+  """
+  with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_prediction'):
+      prediction = tf.argmax(result_tensor, 1)
+      correct_prediction = tf.equal(
+          prediction, tf.argmax(ground_truth_tensor, 1))
+    with tf.name_scope('accuracy'):
+      evaluation_step = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  tf.summary.scalar('accuracy', evaluation_step)
+  return evaluation_step, prediction
+
+def add_jpeg_decoding(input_width, input_height, input_depth):
+  jpeg_data = tf.placeholder(tf.string, name='DecodeJPGInput')
+  decoded_image = tf.image.decode_jpeg(jpeg_data, channels=input_depth)
+  image_as_float = tf.cast(decoded_image, dtype = tf.float32)
+  resize_shape = tf.stack([input_height, input_width])
+  resize_shape_as_int = tf.cast(resize_shape, dtype = tf.int32)
+  image_4d = tf.expand_dims(image_as_float, 0)
+  resized_image = tf.image.resize_bilinear(image_4d,
+                                           resize_shape_as_int)
+  mean = tf.reduce_mean(resized_image)
+  squared_diffs = tf.squared_difference(resized_image, mean)
+  std = tf.sqrt(tf.reduce_mean(squared_diffs)) 
+  offset_image = tf.subtract(resized_image, mean)
+  mul_image = tf.multiply(offset_image, 1.0 / std)
+  return jpeg_data, mul_image
+
+def add_jpeg_grayscale_decoding(input_width, input_height):
+  jpeg_data = tf.placeholder(tf.string, name='DecodeJPGInput')
+  decoded_image = tf.image.decode_jpeg(jpeg_data, channels=1)
+  image_3c = tf.image.grayscale_to_rgb(decoded_image)
+  image_as_float = tf.cast(image_3c, dtype=tf.float32)
+  resize_shape = tf.stack([input_height, input_width])
+  resize_shape_as_int = tf.cast(resize_shape, dtype=tf.int32)
+  image_4d = tf.expand_dims(image_as_float, 0)
+  resized_image = tf.image.resize_bilinear(image_4d,
+                                           resize_shape_as_int)
+  mean = tf.reduce_mean(resized_image)
+  squared_diffs = tf.squared_difference(resized_image, mean)
+  std = tf.sqrt(tf.reduce_mean(squared_diffs)) 
+  offset_image = tf.subtract(resized_image, mean)
+  mul_image = tf.multiply(offset_image, 1.0 / std)
+
+  return jpeg_data, mul_image
+
+def load_graph(model_file):
+  graph = tf.Graph()
+  graph_def = tf.GraphDef()
+
+  with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+  with graph.as_default():
+    tf.import_graph_def(graph_def, name="")
+
+  return graph
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+				input_mean=0, input_std=255):
+  input_name = "file_reader"
+  output_name = "normalized"
+  file_reader = tf.read_file(file_name, input_name)
+  if file_name.endswith(".png"):
+    image_reader = tf.image.decode_png(file_reader, channels = 3,
+                                       name='png_reader')
+  elif file_name.endswith(".gif"):
+    image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                  name='gif_reader'))
+  elif file_name.endswith(".bmp"):
+    image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+  else:
+    image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
+                                        name='jpeg_reader')
+  float_caster = tf.cast(image_reader, tf.float32)
+  dims_expander = tf.expand_dims(float_caster, 0);
+  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+  sess = tf.Session()
+  result = sess.run(normalized)
+
+  return result
+
+def create_image_lists(fname, test_rate, val_rate):
+    image_lists = {}
+    random.seed(1)
+    with open(fname, 'r') as f:
+        for line in f.readlines():
+            toks = line.split()
+            name = toks[0]
+            class_id = toks[1]
+            if class_id not in image_lists:
+                image_lists[class_id]={
+                    'train' : list(),
+                    'test' : list(),
+                    'val' : list()
+                }
+
+            rval = random.randrange(100) * 0.01
+
+            if rval < val_rate:
+                image_lists[class_id]['val'].append(name)
+            elif rval < val_rate + test_rate:
+                image_lists[class_id]['test'].append(name)
+            else:
+                image_lists[class_id]['train'].append(name)
+            
+    return image_lists
+
+def write_out_image_lists(image_lists, ftrain, ftest, fval):
+    flist = {'train' : ftrain, 'test' : ftest, 'val' : fval}
+    for dtype, fname in flist.items():
+        with open(fname, 'w') as f:
+            for class_id, image_list in image_lists.items():
+                for image in image_list['train']:
+                    f.write(image+' '+ str(class_id)+'\n')
+
+def sort_image_lists_by_class_id(image_lists):
+    class_ids = []
+    for k, v in image_lists.items():
+        class_ids.append(int(k))
+
+    sorted_indexes = np.argsort(class_ids)
+    sorted_image_lists = {}
+    for index in sorted_indexes:
+        class_id = class_ids[index]
+        sorted_image_lists[class_id] = image_lists[str(class_id)]
+
+    #for k in sorted_image_lists.keys():
+    #  print(k)
+      
+    return sorted_image_lists
+
+
+def get_image_batch(batch_size, itype, image_dir, image_lists,
+                    decoded_image_tensor, jpeg_data_tensor,
+                    bottleneck_tensor, resized_input_tensor, sess):
+  class_count = len(image_lists.keys())
+  ground_truths = []
+  bottlenecks = []
+  if batch_size < 0:
+    for class_id in image_lists.keys():
+      for image_name in image_lists[class_id][itype]:
+        image_path = os.path.join(image_dir, image_name)
+        if not gfile.Exists(image_path):
+          tf.logging.error('File does not exist {}'.format(image_path))
+        jpeg_data = tf.gfile.FastGFile(image_path, 'rb').read()
+        resized_image_data = sess.run(decoded_image_tensor,
+                                  feed_dict = {jpeg_data_tensor : jpeg_data})
+        bottleneck = sess.run(bottleneck_tensor,
+                            feed_dict = {resized_input_tensor : resized_image_data})
+        bottleneck = np.squeeze(bottleneck)
+        bottlenecks.append(bottleneck)          
+  else:
+    for i in range(batch_size):
+      class_index = random.randrange(class_count)
+      class_id = list(image_lists.keys())[class_index]
+      ground_truth = np.zeros(class_count, dtype=np.float32)
+      ground_truth[class_index] = 1.0
+      ground_truths.append(ground_truth)
+
+      image_index = random.randrange(len(image_lists[class_id][itype]))
+      image_name = image_lists[class_id][itype][image_index]
+      image_path = os.path.join(image_dir, image_name)
+
+      if not gfile.Exists(image_path):
+        tf.logging.error('File does not exist {}'.format(image_path))
+      jpeg_data = tf.gfile.FastGFile(image_path, 'rb').read()
+      resized_image_data = sess.run(decoded_image_tensor,
+                                  feed_dict = {jpeg_data_tensor : jpeg_data})
+      bottleneck = sess.run(bottleneck_tensor,
+                            feed_dict = {resized_input_tensor : resized_image_data})
+      bottleneck = np.squeeze(bottleneck)
+      bottlenecks.append(bottleneck)
+
+  return  bottlenecks, ground_truths
+
+def save_graph_to_file(sess, graph, graph_file_name, final_tensor_name):
+  output_graph_def = graph_util.convert_variables_to_constants(
+      sess, graph.as_graph_def(), [final_tensor_name])
+  with gfile.FastGFile(graph_file_name, 'wb') as f:
+    f.write(output_graph_def.SerializeToString())
+  return
+
+def load_labels(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+  return label
+
+def normalize(v):
+  sum = 0
+  for i in range(len(v)):
+    sum += v[i]
+
+  for i in range(len(v)):
+    v[i] /= sum
+  return v
+
+def remove_invalid_labels(results, labels, invalid_labels):
+  invalid_idxs = list()
+  for invalid_label in invalid_labels:
+    try:
+      idx = labels.index(invalid_label)
+      labels.pop(idx)
+      results = np.delete(results, idx)
+      print(invalid_label, "is removed.")
+    except ValueError as e:
+      print("labels doesn't contain", invalid_label, ".")
+
+  results = normalize(results)
+
+  return results, labels
